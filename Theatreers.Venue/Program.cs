bd85b4b25d72c61349b0bb1d47ae700fdc69b3ef
Christian Reddington <christian.reddington@microsoft.com>
2018-11-27 13:47:31 +0000

    Initialise App Insights for Venue API

--- Theatreers.Venue/Program.cs
+++ Theatreers.Venue/Program.cs
@@ -19,6 +19,7 @@ namespace Theatreers.Venue
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseApplicationInsights();
     }
 }
