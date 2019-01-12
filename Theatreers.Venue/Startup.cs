c5165fbfd31590920380eaf52b371a7c3d7416c3
Christian Reddington <christian.reddington@microsoft.com>
2019-01-12 11:08:48 +0000

    Include venues in health path

--- Theatreers.Venue/Startup.cs
+++ Theatreers.Venue/Startup.cs
@@ -36,7 +36,7 @@ namespace Theatreers.Venue
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app.UseHealthChecks("/health");
+            app.UseHealthChecks("/venues/health");
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
